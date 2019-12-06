6d3d37d0c2d5274173853d45901461cb569080f5
Prashant2411 <prashantbedi241197@gmail.com>
2019-12-06 19:11:35 +0530

    Added: Calculating part time daily wage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -5,12 +5,18 @@ echo "Welcome to Employee Wage Computation Program"
 WAGE_PER_HOUR=20
 fullDayWorkingHour=8
 halfDayWorkingHour=4
-isPresent=$(( RANDOM%2 ))
-if [ $isPresent -eq 1 ]
-then
-	echo "Present"
-	dailyWage=$(( $WAGE_PER_HOUR * $fullDayWorkingHour ))
-	echo "$dailyWage"
-else
-	echo "Absent"
-fi
+FULL_DAY=2
+HALF_DAY=1
+isPresent=$(( RANDOM%3 ))
+case $isPresent in
+	$FULL_DAY )
+		echo "Full Day"
+		dailyWage=$(( $WAGE_PER_HOUR * $fullDayWorkingHour ))
+		echo "Daily Wage: $dailyWage";;
+	$HALF_DAY )
+		echo "Half Day"
+		dailyWage=$(( $WAGE_PER_HOUR * $halfDayWorkingHour ))
+		echo "Daily Wage: $dailyWage";;
+	* )
+		echo "Absent";;
+esac
