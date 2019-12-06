46334a83bd8695563667eaea1f6e95ba1bfc5f31
Prashant2411 <prashantbedi241197@gmail.com>
2019-12-06 18:37:20 +0530

    Added: Calculating daily wage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -2,10 +2,14 @@
 
 echo "Welcome to Employee Wage Computation Program"
 
+WAGE_PER_HOUR=20
+workingHour=8
 isPresent=$(( RANDOM%2 ))
 if [ $isPresent -eq 1 ]
 then
 	echo "Present"
+	dailyWage=$(( $WAGE_PER_HOUR * $workingHour ))
+	echo "$dailyWage"
 else
 	echo "Absent"
 fi
