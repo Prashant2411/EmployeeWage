a8ec615a7e8f965942f4037e9a2772ab4cfb2efd
Prashant2411 <prashantbedi241197@gmail.com>
2019-12-06 18:48:57 +0530

    Added: Part time working hour

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -3,12 +3,13 @@
 echo "Welcome to Employee Wage Computation Program"
 
 WAGE_PER_HOUR=20
-workingHour=8
+fullDayWorkingHour=8
+halfDayWorkingHour=4
 isPresent=$(( RANDOM%2 ))
 if [ $isPresent -eq 1 ]
 then
 	echo "Present"
-	dailyWage=$(( $WAGE_PER_HOUR * $workingHour ))
+	dailyWage=$(( $WAGE_PER_HOUR * $fullDayWorkingHour ))
 	echo "$dailyWage"
 else
 	echo "Absent"
