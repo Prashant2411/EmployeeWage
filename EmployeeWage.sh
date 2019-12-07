16e3d607aff7ac615b88f8ff86a517bc4c0ccdc0
Prashant2411 <prashantbedi241197@gmail.com>
2019-12-07 12:43:55 +0530

    Added: Calculating daily wage for 20 Days

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -7,16 +7,23 @@ fullDayWorkingHour=8
 halfDayWorkingHour=4
 FULL_DAY=2
 HALF_DAY=1
-isPresent=$(( RANDOM%3 ))
-case $isPresent in
-	$FULL_DAY )
-		echo "Full Day"
-		dailyWage=$(( $WAGE_PER_HOUR * $fullDayWorkingHour ))
-		echo "Daily Wage: $dailyWage";;
-	$HALF_DAY )
-		echo "Half Day"
-		dailyWage=$(( $WAGE_PER_HOUR * $halfDayWorkingHour ))
-		echo "Daily Wage: $dailyWage";;
-	* )
-		echo "Absent";;
-esac
+day=1
+while (( $day <= 20 ))
+do
+	isPresent=$(( RANDOM%3 ))
+	case $isPresent in
+		$FULL_DAY )
+			echo "Full Day"
+			dailyWage=$(( $WAGE_PER_HOUR * $fullDayWorkingHour ))
+			echo "Daily Wage: $dailyWage";;
+		$HALF_DAY )
+			echo "Half Day"
+			dailyWage=$(( $WAGE_PER_HOUR * $halfDayWorkingHour ))
+			echo "Daily Wage: $dailyWage";;
+		* )
+			echo "Absent"
+			dailyWage=0
+			echo "Daily Wage: $dailyWage";;
+	esac
+	day=$(( $day + 1 ))
+done
