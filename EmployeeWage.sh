d92a2486932993a6c68e60a2ca2d9028de610b00
Prashant2411 <prashantbedi241197@gmail.com>
2019-12-07 15:29:27 +0530

    Added: Array to store daily and total wage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -19,18 +19,29 @@ function getWorkHour () {
 		* )
 			workingHour=0;;
 	esac
-	dailyWage=$(( $WAGE_PER_HOUR * $workingHour ))
 	echo $workingHour
 }
+function getDailyWage () {
+	dailyWage=$(( $WAGE_PER_HOUR * $1 ))
+	echo $dailyWage
+}
+
+
+
 
 function main () {
 	while (( $day <= 20 && $totalHrs < 100 ))
 	do
 		workingHour=$(getWorkHour)
 		totalHrs=$(( $totalHrs + $workingHour ))
+		dailyWage=$(getDailyWage $workingHour)
+		dailyWageArr[$day]=$dailyWage
+		totalWage=$(( $WAGE_PER_HOUR * $totalHrs ))
+		totalWageArr[$day]=$totalWage
 		day=$(( $day + 1 ))
 	done
-	echo $totalHrs
+	echo ${totalWageArr[@]} 
+	echo ${dailyWageArr[@]}
 }
 
 main
